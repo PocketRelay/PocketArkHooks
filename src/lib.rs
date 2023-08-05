5b53665a3fda41cc5630474854af21b52c12fe9a
Jacobtread <jacobtread@gmail.com>
2023-08-05 17:05:28 +1200

    Swapped plugin loading to after hooks and use new plugin loader

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
 #![allow(clippy::missing_safety_doc)]
 
-use std::{fs, path::Path};
-use windows_sys::Win32::{
-    Foundation::GetLastError,
-    System::{
-        Console::{AllocConsole, FreeConsole},
-        LibraryLoader::{FreeLibrary, LoadLibraryA},
-        SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
-    },
+use windows_sys::Win32::System::{
+    Console::{AllocConsole, FreeConsole},
+    LibraryLoader::FreeLibrary,
+    SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
 };
 
 pub mod hooks;
+pub mod plugin;
 pub mod proxy;
 
 #[no_mangle]
@@ -21,13 +18,14 @@ unsafe extern "system" fn DllMain(dll_module: usize, call_reason: u32, _: *mut (
             // Allocate a console
             AllocConsole();
 
-            // Load ASI plugins
-            load_plugins();
-
             // initialize the proxy
             proxy::init();
+
             // Handles the DLL being attached to the game
             unsafe { hooks::hook() };
+
+            // Load ASI plugins
+            plugin::load();
         }
         DLL_PROCESS_DETACH => {
             // free the proxied library
@@ -42,62 +40,4 @@ unsafe extern "system" fn DllMain(dll_module: usize, call_reason: u32, _: *mut (
     }
 
     true
-}
-
-fn load_plugins() {
-    let path = Path::new("ASI");
-    if !path.exists() {
-        println!("No ASI plugins loaded");
-        return;
-    }
-
-    let files = match fs::read_dir(path) {
-        Ok(value) => value,
-        Err(err) => {
-            eprintln!("Failed to read ASI directory: {}", err);
-            return;
-        }
-    };
-
-    for file in files {
-        let file = match file {
-            Ok(value) => value,
-            Err(err) => {
-                eprintln!("Failed to acccess dir entry: {}", err);
-                continue;
-            }
-        };
-        let is_file = match file.metadata() {
-            Ok(value) => value.is_file(),
-            Err(err) => {
-                eprintln!("Failed to acccess entry metadata: {}", err);
-                continue;
-            }
-        };
-        if !is_file {
-            continue;
-        }
-        let name = file.file_name();
-        let name = match name.to_str() {
-            Some(value) => value,
-            None => continue,
-        };
-
-        if !name.ends_with(".asi") {
-            continue;
-        }
-
-        let mut path = file.path().to_string_lossy().to_string();
-        path.push('\0');
-
-        println!("Loading ASI plugin: {}", name);
-
-        let handle = unsafe { LoadLibraryA(path.as_ptr()) };
-        if handle == 0 {
-            let err = unsafe { GetLastError() };
-            eprintln!("Failed to load library: {}", err);
-        } else {
-            println!("Loaded plugin: {}", name);
-        }
-    }
 }
